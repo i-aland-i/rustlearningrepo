1399cb537b479fe4c6593291fcda285eae1d097c
Aland Aram Mahmood <83509226+i-aland-i@users.noreply.github.com>
2026-03-05 23:55:30 +0300

    Nothing much

--- zerotwo/src/main.rs
+++ zerotwo/src/main.rs
@@ -1,5 +1,15 @@
 fn main() {
     println!("App Started!");
+    println!("");
 
+    let x = 5;
+    let mut y = 20;
+    println!("{}", x);
+
+    println!("{}", y);
+    y += x;
+    println!("{}", y);
+
+    println!("");
     println!("Finished!");
 }
