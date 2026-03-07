997f22a2407efae803d4df6ed05b2bd40d8cc8c0
Aland Aram Mahmood <83509226+i-aland-i@users.noreply.github.com>
2026-03-07 22:41:37 +0300

    Before exercism

--- zerotwo/src/main.rs
+++ zerotwo/src/main.rs
@@ -2,14 +2,15 @@ fn main() {
     println!("App Started!");
     println!("");
 
+    let y = 20;
     let x = 5;
-    let mut y = 20;
-    println!("{}", x);
 
-    println!("{}", y);
-    y += x;
-    println!("{}", y);
+    println!("{}", sum(x, y));
 
     println!("");
     println!("Finished!");
+}
+
+fn sum(x: i32, y: i32) -> i32 {
+    x + y
 }
